f9b3b06f23c45ef526ae9cb5fe8d90c46c7298ea
cherrycrush <phobosnz@gmail.com>
2021-06-16 14:26:00 +1200

    Create routes inside App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import Nav from './Nav'
+import Index from './Index'
 
 const App = () => {
+  return (
+    <div>
+      <Router >
+        <Route path={'/'} component={Nav} />
+        <Switch>
+          <Route exact path={'/'} component={Index} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
