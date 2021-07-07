41e0e14bdc83d717b9fc0c8398cfce7a05abbeb3
cherrycrush <phobosnz@gmail.com>
2021-07-07 14:54:47 +1200

    Add react-reveal to project

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import Bounce from 'react-reveal/Bounce'
 
 const Home = () => {
   return (
     <>
-      <main className='content'>
-        <header className='home-header'>
-          <h1 className='header-title'>
-            <img className='avatar' src='/images/mug.png' alt='avatar'></img>
-            <br></br>
-            Aaron Emerson
-          </h1>
-          <p>Full Stack Web Developer</p>
-        </header>
-        <br></br>
-        <p>Kia ora, I'm Aaron. Currently making the leap into a new career in full stack web development. When I'm not working I'm outside, doing resistance training in the gym, collecting/restoring worn dress shoes and boots or hanging out with friends. I also like to game, used to play Quake Live competitively.</p>
-      </main>
+      <Bounce bottom delay={500}>
+        <main className='content'>
+          <header className='home-header'>
+            <h1 className='header-title'>
+              <img className='avatar' src='/images/mug.png' alt='avatar'></img>
+              <br></br>
+              Aaron Emerson
+            </h1>
+            <p>Full Stack Web Developer</p>
+          </header>
+          <br></br>
+          <p>Kia ora, I'm Aaron. Currently making the leap into a new career in full stack web development. When I'm not working I'm outside, doing resistance training in the gym, collecting/restoring worn dress shoes and boots or hanging out with friends. I also like to game, used to play Quake Live competitively.</p>
+        </main>
+      </Bounce>
     </>
   )
 }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Bounce from 'react-reveal/Bounce'
 
 const Nav = (props) => {
   return (
     <div>
-      <ul className='nav'>
-        <div className='nav-left'>
-          <li>
-            <Link className='nav-item' to ={'/'}>{props.author}</Link>
-          </li>
-        </div>
-        <div className='nav-right'>
-          <li>
-            <Link className='nav-item' to ={'/Blog'}>Blog</Link>
-          </li>
-          <li>
-            <Link className='nav-item' to ={'/Projects'}>Projects</Link>
-          </li>
-          <li>
-            <Link className='nav-item' to ={'/Contact'}>Contact</Link>
-          </li>
-        </div>
-      </ul>
+      <Bounce left delay={200}>
+        <ul className='nav'>
+          <div className='nav-left'>
+            <li>
+              <Link className='nav-item' to ={'/'}>{props.author}</Link>
+            </li>
+          </div>
+          <div className='nav-right'>
+            <li>
+              <Link className='nav-item' to ={'/Blog'}>Blog</Link>
+            </li>
+            <li>
+              <Link className='nav-item' to ={'/Projects'}>Projects</Link>
+            </li>
+            <li>
+              <Link className='nav-item' to ={'/Contact'}>Contact</Link>
+            </li>
+          </div>
+        </ul>
+      </Bounce>
     </div>
   )
 }
