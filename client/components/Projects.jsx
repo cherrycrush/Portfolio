280a248c47931972b3038ac367c7494f24aec59d
cherrycrush <phobosnz@gmail.com>
2021-07-22 23:36:13 +1200

    Add project thumbnail

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -7,8 +7,12 @@ const Projects = () => {
       <Bounce bottom delay={500}>
         <main className='content'>
           <section className='image-card'>
-            <img className='thumbnail' src='#' alt='Card image'></img>
-            <h1 className='title'>Starships in React</h1>
+            <img className='thumbnail' src='./images/react-sw.png' alt='Card image'></img>
+            <h2 className='title'>Starships in React</h2>
+            <br></br>
+            <p className='description'>
+              A project I made during bootcamp that calls an external API and retrieves the data via JSON and displays it inside react components.
+            </p>
           </section>
         </main>
       </Bounce>
