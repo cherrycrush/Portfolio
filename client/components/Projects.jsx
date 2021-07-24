2120b91240b866beb2000e3c2aa24176b6cfee8f
cherrycrush <phobosnz@gmail.com>
2021-07-24 20:55:53 +1200

    Re-position project card elements

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -11,29 +11,31 @@ const Projects = () => {
         <main className='content'>
           <h1>Projects</h1>
           <section className='image-card'>
+            <h2 className='title'>React Star Wars</h2>
             <img className='thumbnail' src='./images/react-sw.png' alt='Project thumbnail'></img>
-            <h2 className='title'>Starships in React</h2>
+            <br></br>
+            <p className='description'>
+              Inspired by Star Wars! A React app that calls an external API (swapi) and retrieves the JSON data and displays that data inside React components.
+            </p>
             <div className='container'>
               {swProject.map((item, index) => (
                 <div key={index} className='info-box'>{item}</div>
               ))}
             </div>
-            <p className='description'>
-              Inspired by Star Wars! A React app that calls an external API (swapi) and retrieves the JSON data and displays that data inside React components.
-            </p>
           </section>
           <hr></hr>
           <section className='image-card'>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/1KplKq0V_Kg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <h2 className='title'>Loyalty Card Wallet</h2>
+            <iframe width="350" height="300" src="https://www.youtube.com/embed/1KplKq0V_Kg" allow="clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <br></br>
+            <p className='description'>
+              A project I created with a group at EDA. An idea was pitched to make a loyalty card app wallet that stores your personal loyalty cards in one place! I helped create the front-end using Semantic UI and designing some of the React components.
+            </p>
             <div className='container'>
               {loyaltyProject.map((item, index) => (
                 <div key={index} className='info-box'>{item}</div>
               ))}
             </div>
-            <p className='description'>
-              A project I created with a group at EDA. An idea was pitched to make a loyalty card app wallet that stores your personal loyalty cards in one place! I helped create the front-end using Semantic UI and designing some of the React components.
-            </p>
           </section>
         </main>
       </Bounce>
